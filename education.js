9e98baf1ebca4284080239cd8bbc7593aff4a704
Marion Vives <mvives.dev@gmail.com>
2024-11-06 14:02:36 +0100

    ajout popover (education)

--- education.js
+++ education.js
@@ -1,10 +1,37 @@
-const popoverClass = document.querySelector(".popover-class");
-const popoverHidden = document.querySelector(".popover-hidden");
-console.log("merde");
+const popoverClass2024 = document.querySelector(".popover-class2024");
+const popoverHidden2024 = document.querySelector(".popover-hidden2024");
+const popoverClass2019 = document.querySelector(".popover-class2019");
+const popoverHidden2019 = document.querySelector(".popover-hidden2019");
+const popoverClass20192 = document.querySelector(".popover-class20192");
+const popoverHidden20192 = document.querySelector(".popover-hidden20192");
+const popoverClass2015 = document.querySelector(".popover-class2015");
+const popoverHidden2015 = document.querySelector(".popover-hidden2015");
+const popoverClass2013 = document.querySelector(".popover-class2013");
+const popoverHidden2013 = document.querySelector(".popover-hidden2013");
+const popoverClass2012 = document.querySelector(".popover-class2012");
+const popoverHidden2012 = document.querySelector(".popover-hidden2012");
+// console.log("coucou");
 
-popoverClass.addEventListener("click", (event) => {
-    popoverHidden.classList.toggle("popover-hidden");
+popoverClass2024.addEventListener("click", (event) => {
+    popoverHidden2024.classList.toggle("popover-hidden2024");
 });
 
-// toto.style.display = "none";
-// element.style.display = "";
+popoverClass2019.addEventListener("click", (event) => {
+    popoverHidden2019.classList.toggle("popover-hidden2019");
+});
+
+popoverClass20192.addEventListener("click", (event) => {
+    popoverHidden20192.classList.toggle("popover-hidden20192");
+});
+
+popoverClass2015.addEventListener("click", (event) => {
+    popoverHidden2015.classList.toggle("popover-hidden2015");
+});
+
+popoverClass2013.addEventListener("click", (event) => {
+    popoverHidden2013.classList.toggle("popover-hidden2013");
+});
+
+popoverClass2012.addEventListener("click", (event) => {
+    popoverHidden2012.classList.toggle("popover-hidden2012");
+});
